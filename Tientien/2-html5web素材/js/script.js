b1f122a3cfe7f21f54b8b1eaad4123de009ef806
bogeelin <bogee311@gmail.com>
2024-08-12 18:14:23 +0800

    0812-18:00

--- Tientien/2-html5web素材/js/script.js
+++ Tientien/2-html5web素材/js/script.js
@@ -7,6 +7,13 @@ $(function(){    /* document ready 一個js檔只要執行一次即可 */
     // 交替漢堡按鈕
     $('.hamburger').click(function(){
         $(this).toggleClass('is-active');
+        $('.navigation').toggleClass('show');
     });
-
+    
+    //滑動至指定位置
+    $('.menu a').click(function () {
+        let btn=$(this).attr('href');  //返回取得屬性與值
+        let pos=$(btn).offset();  //抓取相對的座標位置
+        $('html,body').animate({scrollTop:pos.top},1000);
+    })
 });
