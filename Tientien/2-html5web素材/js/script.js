e965cc6d5898f6d1fbb3260e13e3bbb44cc9a161
bogeelin <bogee311@gmail.com>
2024-08-12 14:40:02 +0800

    0812-15:00

--- Tientien/2-html5web素材/js/script.js
+++ Tientien/2-html5web素材/js/script.js
@@ -1,6 +1,12 @@
-// 移除#about的背景影片
-$(function(){
+$(function(){    /* document ready 一個js檔只要執行一次即可 */
+    // 移除#about的背景影片
     if($(window).width()<821){
         $('#about video').remove();
     }
+
+    // 交替漢堡按鈕
+    $('.hamburger').click(function(){
+        $(this).toggleClass('is-active');
+    });
+
 });
