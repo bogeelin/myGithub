b95cd6683f1e4699f1c1b9364aab6b5395de68cd
bogeelin <bogee311@gmail.com>
2024-08-15 15:02:11 +0800

    0815-15:00

--- Tientien/2-html5web素材/js/script.js
+++ Tientien/2-html5web素材/js/script.js
@@ -1,19 +1,34 @@
-$(function(){    /* document ready 一個js檔只要執行一次即可 */
+$(function () {    /* document ready 一個js檔只要執行一次即可 */
     // 移除#about的背景影片
-    if($(window).width()<821){
+    if ($(window).width() < 821) {
         $('#about video').remove();
     }
 
     // 交替漢堡按鈕
-    $('.hamburger').click(function(){
+    $('.hamburger').click(function () {
         $(this).toggleClass('is-active');
         $('.navigation').toggleClass('show');
     });
-    
+
     //滑動至指定位置
     $('.menu a').click(function () {
-        let btn=$(this).attr('href');  //返回取得屬性與值
-        let pos=$(btn).offset();  //抓取相對的座標位置
-        $('html,body').animate({scrollTop:pos.top},1000);
+        let btn = $(this).attr('href');  //返回取得屬性與值
+        let pos = $(btn).offset();  //抓取相對的座標位置
+        $('html,body').animate({ scrollTop: pos.top }, 1000);
     })
+
+    // 滑動至頂
+    $('#gotop').click(function () {
+        $('html,body').animate({ scrollTop: 0 }, 1000);
+    })
+
+    // 至頂按鈕淡出淡入
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 200) {
+            $('#gotop').stop().fadeTo('', 1);
+        } else {
+            $('#gotop').stop().fadeOut();
+        }
+    });
+
 });
